6281e04e65d3422d3971071e8b9e91b2397e2f0d
Jazlyn Fuller <blackscalemarketing@gmail.com>
2026-01-05 18:01:23 -0800

    Make project gallery images cover containers

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -1151,7 +1151,7 @@ const ProjectDetail = () => {
                             <img
                               src={item.image}
                               alt={item.caption}
-                              className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-105"
+                              className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                             />
                           </div>
                           <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-4 rounded-b-lg">
